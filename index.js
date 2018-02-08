23492e4d97035360fcc222d8c9eeb35d98d5b8b7
Daisuke Hirata <daisukihirata@gmail.com>
2018-02-08 23:28:29 +0900

    add video2

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ const config = {
   messagingSenderId: "xxxxxxxxxxxxxxxx"
 };
 
+/*
+PowerOn, hdmi1, hdmi2, hdmi3, hdmi4, Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9, Num0, Num11, Num12, Enter, GGuide, ChannelUp, ChannelDown, VolumeUp, VolumeDown, Mute, TvPower, Audio, MediaAudioTrack, Tv, Input, TvInput, TvAntennaCable, WakeUp, PowerOff, Sleep, Right, Left, SleepTimer, Analog2, TvAnalog, Display, Jump, PicOff, PictureOff, Teletext, Video1, Video2, AnalogRgb1, Home, Exit, PictureMode, Confirm, Up, Down, ClosedCaption, Component1, Component2, Wide, EPG, PAP, TenKey, BSCS, Ddata, Stop, Pause, Play, Rewind, Forward, DOT, Rec, Return, Blue, Red, Green, Yellow, SubTitle, CS, BS, Digital, Options, Media, Prev, Next, DpadCenter, CursorUp, CursorDown, CursorLeft, CursorRight, ShopRemoteControlForcedDynamic, FlashPlus, FlashMinus, DemoMode, Analog, Mode3D, DigitalToggle, DemoSurround, *AD, AudioMixUp, AudioMixDown, PhotoFrame, Tv_Radio, SyncMenu, Hdmi1, Hdmi2, Hdmi3, Hdmi4, TopMenu, PopUpMenu, OneTouchTimeRec, OneTouchView, DUX, FootballMode, iManual, Netflix, Assists, FeaturedApp, FeaturedAppVOD, GooglePlay, ActionMenu, Help, TvSatellite, WirelessSubwoofer, AndroidMenu
+*/
 const commandTable = {
   "つけ": "PowerOn",
   "オン": "PowerOn",
@@ -31,7 +34,8 @@ const commandTable = {
   "アップ": "VolumeUp",
   "下げ": "VolumeDown",
   "ダウン": "VolumeDown",
-  "ミュート": "Mute"  
+  "ミュート": "Mute",
+  "DVD": "Video2"
 };
 
 firebase.initializeApp( config );
